a736aca92f2ffa236aac9e2bae1dbc80e21cc53e
Alex Howe <howie242000@gmail.com>
2020-12-06 17:38:08 -0500

    OMG POLLS WRITE TO JSON NOW LETSGOOOOOOOOOOO

--- commands/poll.js
+++ commands/poll.js
@@ -5,9 +5,50 @@ module.exports = {
     name: 'Poll',
     command: 'poll',
     description: "Stars a poll using command arguments. Currently broken.",
-    usage: '`poll "[title]" ..."[option]"`',
+    usage: '`poll "[title]" "[option 1]"..."[option n]"`',
     execute(message, args) {
-        if(!args.length) {
+        if(args.length < 2) {
+            message.channel.send(`Usage: ${this.usage}`);
+            return;
+        }
+        console.log(args);
+        // Turn [arg1, arg2, arg3, arg4, ... , argn] (array of words) into [title, option1, option2, ... , optionn] (array of strings)
+        const pollargs = args.reduce(function(acc, val) {
+            console.log(acc);
+            if(val.startsWith("\"")) {
+                if(acc.instring) {
+                    acc.validsyntax = false;
+                }
+                else {
+                    let str = val.slice(1);
+                    if(!val.endsWith("\"")) {
+                        acc.instring = true;
+                    }
+                    else {
+                        str = str.substring(0, str.length-1);
+                    }
+                    acc.data.push(str);
+                    acc.counter += 1;
+                }
+            }
+            else if(val.endsWith("\"")) {
+                if(!acc.instring) {
+                    acc.validsyntax = false;
+                }
+                else {
+                    acc.data[acc.counter] = acc.data[acc.counter].concat(' ' + val.substring(0, val.length-1));
+                    acc.instring = false;
+                }
+            }
+            else {
+                acc.data[acc.counter] = acc.data[acc.counter].concat(' ' + val);
+            }
+            return acc;
+        }, { data: [], counter: -1, instring: false, validsyntax: true });
+
+        console.log(pollargs);
+        // Check if the command had valid syntax
+        if(!pollargs.validsyntax) {
             message.channel.send(`Usage: ${this.usage}`);
             return;
         }
@@ -15,48 +56,14 @@ module.exports = {
             message.channel.send('This channel already has an active poll!');
             return;
         }
-        // Turn [arg1, arg2, arg3, arg4, ... , argn] (array of words) into [title, option1, option2, ... , optionn] (array of strings)
-        let pollargs = args.reduce(function(acc, val) {
-            let counter = acc.counter;
-            if(val.startsWith("\"")) {
-                if(acc.instring) {
-                    acc.validsyntax = false;
-                    return;
-                }
-                else {
-                    acc.data.push(val);
-                    acc.counter += 1;
-                    acc.instring = true;
-                }
-            }
-            else if(val.endsWith("\"")) {
-                if(!acc.instring) {
-                    acc.validsyntax = false;
-                    return;
-                }
-                else {
-                    acc.data[counter] = acc.data[counter].concat(' ' + val);
-                }
-            }
-            else {
-                acc.data[counter] = acc.data[counter].concat(' ' + val);
-            }
-        }, { data: [], counter: -1, instring: false, validsyntax: true });
-
-        console.log(pollargs.data);
-        // Check if the command had valid syntax
-        if(!pollargs.validsyntax) {
-            message.channel.send(`Usage: ${this.usage}`);
-            return;
-        }
 
         let polldata = {
-            title: pollargs.shift()
+            title: pollargs.data.shift()
         };
-        for(let i = 0; i < pollargs.length; i++) {
+        for(let i = 0; i < pollargs.data.length; i++) {
             Object.defineProperty(polldata, i, {
                 value: {
-                    title: pollargs[i],
+                    name: pollargs.data[i],
                     percentage: undefined
                 },
                 writable: true,
@@ -72,6 +79,6 @@ module.exports = {
             enumerable: true
         });
         
-        fs.writeFile('./data/polls.json', JSON.stringify(polljson, null, 4), (err) => { if(err) { throw err; } else { message.channel.send(`Wrote ${polldata.data} to \`polls.json\``) } });
+        fs.writeFile('./data/polls.json', JSON.stringify(polljson, null, 4), (err) => { if(err) { throw err; } else { message.channel.send(`Wrote ${polldata} to \`polls.json\``) } });
     }
 }
